f6fed888c572ecb87a49ddc7557879db7e7adc61
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-05-09 17:57:28 +0800

    Add files

--- 作业2/circle_identity.m
+++ 作业2/circle_identity.m
@@ -11,7 +11,6 @@ pos = d.Position;
 diffPos = diff(pos);
 diameter = hypot(diffPos(1),diffPos(2));
 % 搜索半径在[diameter/2-5 diameter/2+5]左右的圆
-% [centers,radii] = imfindcircles(Orig_img,[int8(diameter/2)-5 int8(diameter/2)+5],'ObjectPolarity','bright', 'Sensitivity',0.97);
 [centers,radii] = imfindcircles(Orig_img,[int8(diameter/2)-5 int8(diameter/2)+5],'ObjectPolarity','dark', 'Sensitivity',0.96);
 imshow(Orig_img);
 h = viscircles(centers,radii);
